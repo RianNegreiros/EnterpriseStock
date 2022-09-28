2513e367343d242349a50a51ddd1b876909e8cc9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-28 17:44:50 -0300

    add listing to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
-
+  :recoverable, :rememberable, :validatable,
+  :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
+  
+  has_many :listings, foreign_key: :host_id
+  
         def self.from_omniauth(auth)
           where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
             user.email = auth.info.email

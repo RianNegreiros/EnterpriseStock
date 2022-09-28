d272e1fd68fcb23736a2777243d68e03ad4ca759
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-28 17:47:34 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
+  namespace :host do
+    resources :listings do
+      resources :rooms, only: [:index, :create, :destroy]
+    end
+  end
+  
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end

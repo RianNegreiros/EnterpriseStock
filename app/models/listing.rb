00a6fea8142cd2ff55f2d956a0a1d8087a3303bf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-29 14:30:00 -0300

    scope and address function

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,9 @@ class Listing < ApplicationRecord
   validates :max_guests, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
   belongs_to :host, class_name: 'User'
   enum status: { draft: 0, published: 1, archived: 2 }
+  scope :published, -> { where(status: :published) }
+
+  def address
+    "#{address_line1} #{address_line2} #{city} #{state}"
+  end
 end

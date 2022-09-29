ef97cbef3524f0470bd2606a876808fd6d8f9629
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-29 09:39:17 -0300

    fix address controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,8 +3,11 @@
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+//import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+//eagerLoadControllersFrom("controllers", application)
+
+import AddressController from "./address_controller"
+application.register("address", AddressController)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"

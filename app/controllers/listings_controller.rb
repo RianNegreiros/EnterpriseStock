b8411e689d9227aee648688c0048099e9a38c725
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-09-30 08:22:37 -0300

    fix filter listing by status

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,9 @@
 class ListingsController < ApplicationController
   def index
-    @listings = Listing.published
+    @listings = Listing.where(status: :published)
   end
 
   def show
-    @listing = Listing.published.find(params[:id])
+    @listing = Listing.where(status: :published).find(params[:id])
   end
 end

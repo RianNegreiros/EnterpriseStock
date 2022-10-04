61ae5ecef3bf1eeadbd865e3aa19a3521f37a776
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-10-04 14:19:48 -0300

    add reservation notification and messages

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ApplicationRecord
   enum status: { pending: 0, booked: 1, cancelled: 2, canceling: 3, expired: 4 }
   has_one :calendar_event, dependent: :destroy, required: true
   has_one :host, through: :listing
+  has_many :messages
 
   delegate :start_date, to: :calendar_event
   delegate :end_date, to: :calendar_event

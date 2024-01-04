11436c4b98161787a640ee152f172aed020bae3e
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-04 09:59:29 -0300

    feat: add pagination

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,14 @@
 INSERT INTO tb_category(name) VALUES ('Books')
 INSERT INTO tb_category(name) VALUES ('Electronics')
 INSERT INTO tb_category(name) VALUES ('Computers')
+INSERT INTO tb_category(name) VALUES ('Clothing');
+INSERT INTO tb_category(name) VALUES ('Home and Garden');
+INSERT INTO tb_category(name) VALUES ('Toys and Games');
+INSERT INTO tb_category(name) VALUES ('Sports and Outdoors');
+INSERT INTO tb_category(name) VALUES ('Beauty and Personal Care');
+INSERT INTO tb_category(name) VALUES ('Health and Wellness');
+INSERT INTO tb_category(name) VALUES ('Automotive');
+INSERT INTO tb_category(name) VALUES ('Appliances');
+INSERT INTO tb_category(name) VALUES ('Movies and Music');
+INSERT INTO tb_category(name) VALUES ('Office Supplies');
+INSERT INTO tb_category(name) VALUES ('Pet Supplies');

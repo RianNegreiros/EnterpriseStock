fcc01729d630a4f444ea6e76d399d84cff478ba8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-04 15:23:30 -0300

    feat: add user resource

---
+++ src/main/java/com/github/RianNegreiros/productcataloging/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.github.RianNegreiros.productcataloging.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .build();
+    }
+
+}
